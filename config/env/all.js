495abc6da02455505369ef4a9f154b21d8486e6a
Kim Carter <Kim.Carter@binarymist.net>
2016-05-15 17:23:52 +1200

    Modify Dockerfile to use node 4.4 and initial config for Zap in container.

--- config/env/all.js
+++ config/env/all.js
@@ -5,5 +5,8 @@ module.exports = {
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256",
-    hostName: "localhost"
+    // hostName as seen by the OWASP Zap container.
+    // This may be different, but once you have the Zap container running,
+    // do an ifconfig on your physical host and you'll see what docker has assigned to your physical host.
+    hostName: "172.17.0.1"
 };
--- config/env/development.js
+++ config/env/development.js
@@ -6,13 +6,19 @@ module.exports = {
    // That will create the local nodegoat data-store, or restore it to a clean state if it already exists.
 
    //db: "mongodb://localhost:27017/nodegoat",
+   db: "mongodb://localhost:27017/nodegoat",
+
 
    // If you want to use a MongoLab instance, just sign up for it, create a data-store, in this example we call it nodegoat.
    // and again just run the grunt db-reset:development command
    //db: 'mongodb://<dbuser>:<dbpassword>@<databasename>',
 
-   // If you want to debug regression tests, you will need the following which is also in the test config:
-   zapHostName: "192.168.56.20",
+   // If you want to debug regression tests, you will need the following which is also in the test config.
+   // If using the Zap docker container, use the IP address that docker assigned to it.
+   // To find this out:
+   // docker ps # will provide the running containers details. Use the container Id in the followng command
+   // docker inspect <container Id> | grep IPAddress # Use the outputted IP.
+   zapHostName: "172.17.0.2",
    zapPort: "8080",
    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
