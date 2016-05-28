11d300c9f441b610c7940b94eee9b475ed80255f
Kim Carter <binarymist@users.noreply.github.com>
2016-05-28 20:58:20 +1200

    Added Comments from branch ContainerisedZap-LocalNodeGoatAndDb

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,12 @@
 // default app configuration
 module.exports = {
-    port: process.env.PORT || 4000,
-    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
-    cookieSecret: "session_cookie_secret_key_here",
-    cryptoKey: "a_secure_key_for_crypto_here",
-    cryptoAlgo: "aes256",
-    hostName: "172.17.0.1"
+   port: process.env.PORT || 4000,
+   db:  process.env.MONGOLAB_URI || process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
+   cookieSecret: "session_cookie_secret_key_here",
+   cryptoKey: "a_secure_key_for_crypto_here",
+   cryptoAlgo: "aes256",
+   // hostName as seen by the OWASP Zap container.
+   // This may be different, but once you have the Zap container running,
+   // do an ifconfig on your physical host and you'll see what docker has assigned to your physical host.
+   hostName: "172.17.0.1"
 };
