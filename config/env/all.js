f1c4ce4fc757f81be1e6b3c7228658f681e29e02
Kim Carter <binarymist@users.noreply.github.com>
2016-05-28 20:58:22 +1200

    Added Comments from branch ContainerisedZap-LocalNodeGoatAndDb

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,10 @@
 // default app configuration
 module.exports = {
-    port: process.env.PORT || 4000,
-    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
-    cookieSecret: "session_cookie_secret_key_here",
-    cryptoKey: "a_secure_key_for_crypto_here",
-    cryptoAlgo: "aes256",
-    hostName: "192.168.56.1"
+   port: process.env.PORT || 4000,
+   db:  process.env.MONGOLAB_URI || process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
+   cookieSecret: "session_cookie_secret_key_here",
+   cryptoKey: "a_secure_key_for_crypto_here",
+   cryptoAlgo: "aes256",
+   // hostName as seen by OWASP Zap
+   hostName: "192.168.56.1"
 };
