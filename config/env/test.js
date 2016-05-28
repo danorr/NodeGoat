b7beeb6a4d27e8d5c0c4d23010bf9c24f710b248
Kim Carter <binarymist@users.noreply.github.com>
2016-05-28 20:52:03 +1200

    Added Comments from branch ContainerisedZap-LocalNodeGoatAndDb

--- config/env/test.js
+++ config/env/test.js
@@ -1,8 +1,15 @@
 module.exports = {
-    //port: 5051
-    zapHostName: "172.17.0.2",
-    zapPort: "8080",
-    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
-    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
-    zapApiFeedbackSpeed: 5000 // Milliseconds.
+   // If you want to debug regression tests, you will need the following.
+   // If using the Zap docker container, use the IP address that docker assigned to it.
+   // To find this out:
+   // docker ps # will provide the running containers details. Use the container Id in the followng command
+   // docker inspect <container Id> | grep IPAddress # Use the outputted IP.
+    
+   zapHostName: "172.17.0.2",
+   // Otherwise if running Zap Api in the virtualbox VM, use something like the following:
+   // zapHostName: "192.168.56.20",
+   zapPort: "8080",
+   // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
+   zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
+   zapApiFeedbackSpeed: 5000 // Milliseconds.
 };
